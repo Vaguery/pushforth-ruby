b5c88788141eff9917b3be462a55f32301872083
Bill Tozier <bill@vagueinnovation.com>
2015-04-03 07:01:42 -0400

    cleaned up and fixed bugs in comparison functions

--- lib/pf-comparison.rb
+++ lib/pf-comparison.rb
@@ -3,56 +3,75 @@ module PushForth
 
     ### comparison 
 
-    def >(stack)
-      return arithmetic(:>, stack) do |a,b|
-        if a.kind_of?(Complex) || b.kind_of?(Complex)
-          Error.new("compared Complex values")
+    def ==(stack)
+      if stack.length > 2
+        code = stack.shift
+        arg1,arg2 = stack.shift(2)
+        stack.unshift(arg1==arg2)
+        stack.unshift(code)
+      end
+      return stack
+    end
+
+    def ≠(stack)
+      if stack.length > 2
+        code = stack.shift
+        arg1,arg2 = stack.shift(2)
+        stack.unshift(arg1!=arg2)
+        stack.unshift(code)
+      end
+      return stack
+    end
+
+
+    def comparable?(thing)
+      !thing.kind_of?(Symbol) && !thing.kind_of?(Complex) && thing.class.include?(Comparable) 
+    end
+
+    def compare(instruction, stack, &math_proc)
+      unless stack.length < 3
+        code = stack.shift
+        arg1, arg2 = stack.shift(2)
+        k1 = pushforth_type(arg1)
+        k2 = pushforth_type(arg2)
+        compy = comparable?(arg1)
+        if compy && k1==k2
+          stack.unshift(math_proc.call(arg1,arg2))
+        elsif compy
+          code.unshift(instruction,arg2)
+          stack.unshift(arg1)
         else
-           a > b
+          code.unshift(instruction,arg1)
+          stack.unshift(arg2)
         end
+        stack.unshift(code)
+      end
+      return stack
+    end
+
+    def >(stack)
+      return compare(:>, stack) do |a,b|
+        a > b
       end
     end
 
     def <(stack)
-      return arithmetic(:<, stack) do |a,b|
-        if a.kind_of?(Complex) || b.kind_of?(Complex)
-          Error.new("compared Complex values")
-        else
-           a > b
-        end
+      return compare(:<, stack) do |a,b|
+        a < b
       end
     end
 
     def ≥(stack)
-      return arithmetic(:≥, stack) do |a,b|
-        if a.kind_of?(Complex) || b.kind_of?(Complex)
-          Error.new("compared Complex values")
-        else
-           a >= b
-        end
+      return compare(:≥, stack) do |a,b|
+        a >= b
       end
     end
 
     def ≤(stack)
-      return arithmetic(:≤, stack) do |a,b|
-        if a.kind_of?(Complex) || b.kind_of?(Complex)
-          Error.new("compared Complex values")
-        else
-           a <= b
-        end
+      return compare(:≤, stack) do |a,b|
+        a <= b
       end
     end
 
-    def ==(stack)
-      return arithmetic(:==, stack) do |a,b|
-        a == b
-      end
-    end
-
-    def ≠(stack)
-      return arithmetic(:≠, stack) do |a,b|
-        a != b
-      end
-    end
   end
 end
--- spec/comparison_spec.rb
+++ spec/comparison_spec.rb
@@ -2,70 +2,63 @@ require 'spec_helper'
 
 describe "comparison" do
   describe ">" do
-    it "should be a recognized instruction" do
-      expect(PushForthInterpreter.new.instruction?(:>)).to be true
-    end
-
-    it "should disappear if there are not two Numeric args" do
+    it "should disappear if there are not two arguments" do
       expect(PushForthInterpreter.new([[:>]]).step!.stack).to eq [[]]
       expect(PushForthInterpreter.new([[:>],1]).step!.stack).to eq [[],1]
     end
 
-    it "should return a Boolean if there are two Numerics there" do
+    it "should return a Boolean if there are two comparable items of the same type there" do
       expect(PushForthInterpreter.new([[:>],1,2]).step!.stack).to eq [[],false]
       expect(PushForthInterpreter.new([[:>],1,1]).step!.stack).to eq [[],false]
       expect(PushForthInterpreter.new([[:>],3,2]).step!.stack).to eq [[],true]
-      expect(PushForthInterpreter.new([[:>],Rational("1/4"),0.124]).step!.stack).to eq [[],true]
+      expect(PushForthInterpreter.new([[:>],0.25r,0.22r]).step!.stack).to eq [[],true]
     end
 
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:>],"a",2,3]).step!.stack).to eq [[:>,"a"],2,3]
-      expect(PushForthInterpreter.new([[:>],2,"b",3]).step!.stack).to eq [[:>,"b"],2,3]
+    it "should build a continuation if arg1 isn't comparable" do
+      expect(PushForthInterpreter.new([[:>],:add,2,3]).step!.stack).
+        to eq [[:>,:add],2,3]
+      expect(PushForthInterpreter.new([[:>],:add,:add,3]).step!.stack).to eq [[:>,:add],:add,3]
+    end
+
+    it "should build a continuation if arg2 isn't the same type as arg1" do
+      expect(PushForthInterpreter.new([[:>],2,:add,3]).step!.stack).
+        to eq [[:>,:add],2,3]
     end
 
     it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:>,1,2],"a",3,4]).step!.stack).
-        to eq [[:>,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:>,1,2],3,"a",4]).step!.stack).
-        to eq [[:>,"a",1,2],3,4]
+      expect(PushForthInterpreter.new([[:>,1,2],:add,3,4]).step!.stack).
+        to eq [[:>,:add,1,2],3,4]
+      expect(PushForthInterpreter.new([[:>,1,2],3,:add,4]).step!.stack).
+        to eq [[:>,:add,1,2],3,4]
     end
 
-    it "produce an error if one of the arguments is a Complex number" do
-      expect(PushForthInterpreter.new([[:>],0.25r,-4+1i]).step!.stack[-1]).to be_a_kind_of(Error)
+    it "should not be applied to Complex arguments" do
+      expect(PushForthInterpreter.new([[:>],3+2i,3+1i]).step!.stack).to eq [[:>, (3+2i)], (3+1i)]
     end
   end
 
   describe "<" do
-    it "should be a recognized instruction" do
-      expect(PushForthInterpreter.new.instruction?(:<)).to be true
-    end
-
-    it "should disappear if there are not two Numeric args" do
+    it "should disappear if there are not two args" do
       expect(PushForthInterpreter.new([[:<]]).step!.stack).to eq [[]]
       expect(PushForthInterpreter.new([[:<],1]).step!.stack).to eq [[],1]
     end
 
-    it "should return a Boolean if there are two Numerics there" do
-      expect(PushForthInterpreter.new([[:<],1,2]).step!.stack).to eq [[],false]
+    it "should return a Boolean if arg1 is Comparable & arg2 the same type" do
+      expect(PushForthInterpreter.new([[:<],1,2]).step!.stack).to eq [[],true]
       expect(PushForthInterpreter.new([[:<],1,1]).step!.stack).to eq [[],false]
-      expect(PushForthInterpreter.new([[:<],3,2]).step!.stack).to eq [[],true]
-      expect(PushForthInterpreter.new([[:<],Rational("1/4"),0.124]).step!.stack).to eq [[],true]
+      expect(PushForthInterpreter.new([[:<],3,2]).step!.stack).to eq [[],false]
+      expect(PushForthInterpreter.new([[:<],0.100,0.124]).step!.stack).to eq [[],true]
     end
 
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:<],"a",2,3]).step!.stack).to eq [[:<,"a"],2,3]
-      expect(PushForthInterpreter.new([[:<],2,"b",3]).step!.stack).to eq [[:<,"b"],2,3]
+    it "should build a continuation if arg1 isn't comparable" do
+      expect(PushForthInterpreter.new([[:<],:add,2,3]).step!.stack).
+        to eq [[:<,:add],2,3]
+      expect(PushForthInterpreter.new([[:<],:add,:add,3]).step!.stack).to eq [[:<,:add],:add,3]
     end
 
-    it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:<,1,2],"a",3,4]).step!.stack).
-        to eq [[:<,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:<,1,2],3,"a",4]).step!.stack).
-        to eq [[:<,"a",1,2],3,4]
-    end
-
-    it "produce an error if one of the arguments is a Complex number" do
-      expect(PushForthInterpreter.new([[:<],0.25r,-4+1i]).step!.stack[-1]).to be_a_kind_of(Error)
+    it "should build a continuation if arg2 isn't the same type as arg1" do
+      expect(PushForthInterpreter.new([[:<],2,:add,3]).step!.stack).
+        to eq [[:<,:add],2,3]
     end
   end
 
@@ -83,23 +76,18 @@ describe "comparison" do
       expect(PushForthInterpreter.new([[:≥],1,2]).step!.stack).to eq [[],false]
       expect(PushForthInterpreter.new([[:≥],1,1]).step!.stack).to eq [[],true]
       expect(PushForthInterpreter.new([[:≥],3,2]).step!.stack).to eq [[],true]
-      expect(PushForthInterpreter.new([[:≥],Rational("1/4"),0.124]).step!.stack).to eq [[],true]
+      expect(PushForthInterpreter.new([[:≥],0.2,0.124]).step!.stack).to eq [[],true]
     end
 
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:≥],"a",2,3]).step!.stack).to eq [[:≥,"a"],2,3]
-      expect(PushForthInterpreter.new([[:≥],2,"b",3]).step!.stack).to eq [[:≥,"b"],2,3]
+    it "should build a continuation if arg1 isn't comparable" do
+      expect(PushForthInterpreter.new([[:≥],:add,2,3]).step!.stack).
+        to eq [[:≥,:add],2,3]
+      expect(PushForthInterpreter.new([[:≥],:add,:add,3]).step!.stack).to eq [[:≥,:add],:add,3]
     end
 
-    it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:≥,1,2],"a",3,4]).step!.stack).
-        to eq [[:≥,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:≥,1,2],3,"a",4]).step!.stack).
-        to eq [[:≥,"a",1,2],3,4]
-    end
-
-    it "produce an error if one of the arguments is a Complex number" do
-      expect(PushForthInterpreter.new([[:≥],0.2r,-4+1i]).step!.stack[-1]).to be_a_kind_of(Error)
+    it "should build a continuation if arg2 isn't the same type as arg1" do
+      expect(PushForthInterpreter.new([[:≥],2,:add,3]).step!.stack).
+        to eq [[:≥,:add],2,3]
     end
   end
 
@@ -117,23 +105,18 @@ describe "comparison" do
       expect(PushForthInterpreter.new([[:≤],1,2]).step!.stack).to eq [[],true]
       expect(PushForthInterpreter.new([[:≤],1,1]).step!.stack).to eq [[],true]
       expect(PushForthInterpreter.new([[:≤],3,2]).step!.stack).to eq [[],false]
-      expect(PushForthInterpreter.new([[:≤],Rational("1/4"),0.124]).step!.stack).to eq [[],false]
+      expect(PushForthInterpreter.new([[:≤],0.3,0.124]).step!.stack).to eq [[],false]
     end
 
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:≤],"a",2,3]).step!.stack).to eq [[:≤,"a"],2,3]
-      expect(PushForthInterpreter.new([[:≤],2,"b",3]).step!.stack).to eq [[:≤,"b"],2,3]
+    it "should build a continuation if arg1 isn't comparable" do
+      expect(PushForthInterpreter.new([[:≤],:add,2,3]).step!.stack).
+        to eq [[:≤,:add],2,3]
+      expect(PushForthInterpreter.new([[:≤],:add,:add,3]).step!.stack).to eq [[:≤,:add],:add,3]
     end
 
-    it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:≤,1,2],"a",3,4]).step!.stack).
-        to eq [[:≤,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:≤,1,2],3,"a",4]).step!.stack).
-        to eq [[:≤,"a",1,2],3,4]
-    end
-
-    it "produce an error if one of the arguments is a Complex number" do
-      expect(PushForthInterpreter.new([[:≤],0.3r,2+2i]).step!.stack[-1]).to be_a_kind_of(Error)
+    it "should build a continuation if arg2 isn't the same type as arg1" do
+      expect(PushForthInterpreter.new([[:≤],2,:add,3]).step!.stack).
+        to eq [[:≤,:add],2,3]
     end
   end
 
@@ -151,23 +134,13 @@ describe "comparison" do
       expect(PushForthInterpreter.new([[:==],1,2]).step!.stack).to eq [[],false]
       expect(PushForthInterpreter.new([[:==],1,1]).step!.stack).to eq [[],true]
       expect(PushForthInterpreter.new([[:==],3,2]).step!.stack).to eq [[],false]
-      expect(PushForthInterpreter.new([[:==],Rational("1/4"),0.124]).step!.stack).to eq [[],false]
+      expect(PushForthInterpreter.new([[:==],0.125,0.122]).step!.stack).to eq [[],false]
     end
 
-    it "should work for Complex (unlike <=> operators)" do
-      expect(PushForthInterpreter.new([[:==],0.2r,3+1i]).step!.stack[-1]).to be false
-    end
-
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:==],"a",2,3]).step!.stack).to eq [[:==,"a"],2,3]
-      expect(PushForthInterpreter.new([[:==],2,"b",3]).step!.stack).to eq [[:==,"b"],2,3]
-    end
-
-    it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:==,1,2],"a",3,4]).step!.stack).
-        to eq [[:==,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:==,1,2],3,"a",4]).step!.stack).
-        to eq [[:==,"a",1,2],3,4]
+    it "should work for Complexor List (unlike <=> operators)" do
+      expect(PushForthInterpreter.new([[:==],3+2i,3+1i]).step!.stack).to eq [[], false]
+      expect(PushForthInterpreter.new([[:==],3+1i,3+1i]).step!.stack).to eq [[], true]
+      expect(PushForthInterpreter.new([[:==],[1,2],[1,2]]).step!.stack).to eq [[], true]
     end
   end
 
@@ -189,19 +162,7 @@ describe "comparison" do
     end
 
     it "should work for Complex (unlike <=> operators)" do
-      expect(PushForthInterpreter.new([[:≠],0.2r,1+1i]).step!.stack[-1]).to be true
-    end
-
-    it "should build a continuation if either of the args isn't Numeric" do
-      expect(PushForthInterpreter.new([[:≠],"a",2,3]).step!.stack).to eq [[:≠,"a"],2,3]
-      expect(PushForthInterpreter.new([[:≠],2,"b",3]).step!.stack).to eq [[:≠,"b"],2,3]
-    end
-
-    it "should work when the code stack is populated" do
-      expect(PushForthInterpreter.new([[:≠,1,2],"a",3,4]).step!.stack).
-        to eq [[:≠,"a",1,2],3,4]
-      expect(PushForthInterpreter.new([[:≠,1,2],3,"a",4]).step!.stack).
-        to eq [[:≠,"a",1,2],3,4]
+      expect(PushForthInterpreter.new([[:≠],1+1i,1+1i]).step!.stack).to eq [[], false]
     end
   end
 end
